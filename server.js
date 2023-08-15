e10afbf23289fa524d4ca1833984531e5467e544
lucas matias <lucasmlima1516@gmail.com>
2023-08-15 20:08:23 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const products = require('./data/products')
+
+const server = http.createServer((req, res) => {
+    /*res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hello World</h1>')
+    res.end()*/
+    if (req.url === '/api/products' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Route not found' }))
+    }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
